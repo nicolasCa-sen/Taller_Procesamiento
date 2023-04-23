49572287b8e003541c38bf3ba1020857f0b95c78
Nicolas Cáceres <nicolascaceres@MacBook-Air-de-Nicolas.local>
2023-04-23 16:23:27 -0500

    cambios 4

--- src/main/java/Logic/main.java
+++ src/main/java/Logic/main.java
@@ -17,10 +17,32 @@ public class main {
 
         System.out.println(st.getCons_Gene().size());
         System.out.println(st.getCons_Cardi().size());
+        System.out.println("============================================");
         System.out.println("consulta general");
         for (int i = 0; i <st.getCons_Gene().size() ; i++) {
             System.out.println(st.getCons_Gene().get(i).getCliente().getNombre());
         }
+        System.out.println("============================================");
+        for (int i = 0; i <st.getCons_Gene().size() ; i++) {
+            st.getCons_Gene().get(i).setAtencion(true);
+        }
+        st.turno("juan",428956,"Consulta general");
+        st.turno("nico",428956,"Consulta general");
+        st.turno("numar",428956,"Consulta general");
+        st.turno("juan",428956,"Consulta general");
+        st.turno("juan",428956,"Consulta general");
+        System.out.println("============================================");
+        System.out.println("consulta general");
+        for (int i = 0; i <st.getCons_Gene().size() ; i++) {
+            if (!st.getCons_Gene().get(i).isAtencion()){
+                System.out.println(st.getCons_Gene().get(i).getCliente().getNombre());
+            }
+
+        }
+
+
+
+
 
 
 
